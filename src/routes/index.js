4d4b7be64cb5a7b52683f6efe1e5cc97f4f38461
EugeneKostin <zh.kostin97@gmail.com>
2022-03-13 17:26:49 +0300

    add path constants

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 import { Create } from "../pages/Create";
 import { RecipeDetails } from "../pages/RecipeDetails";
 import { Recipes } from "../pages/Recipes";
+import { PAGE_RECIPES_PATH, PAGE_RECIPES_DEPAILS_PATH, PAGE_CREATE_PATH } from '../utils/constants'
 
 export const routes = [
     {
-        path: '/',
+        path: PAGE_RECIPES_PATH,
         element: <Recipes />,
     },
     {
-        path: '/create',
+        path: PAGE_CREATE_PATH,
         element: <Create />,
     },
     {
-        path: '/recipe/:id',
+        path: PAGE_RECIPES_DEPAILS_PATH,
         element: <RecipeDetails />,
     },
 ];
--- src/utils/menuItems.js
+++ src/utils/menuItems.js
@@ -1,17 +1,19 @@
 import AddIcon from '@mui/icons-material/Add';
 import ListAltIcon from '@mui/icons-material/ListAlt';
+import { PAGE_RECIPES_PATH, PAGE_CREATE_PATH } from '../utils/constants'
+
 
 export const menuItems = [
     {
         id: 0,
         icon: <ListAltIcon />,
         label: 'Рецепты',
-        url: '/',
+        url: PAGE_RECIPES_PATH,
     },
     {
         id: 1,
         icon: <AddIcon />,
         label: 'Создать',
-        url: '/create',
+        url: PAGE_CREATE_PATH,
     },
 ];
